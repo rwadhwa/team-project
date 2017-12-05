e1e320746adfc3bd91c91aa634325bcedbadfca9
Parth Patel <parth44@gmail.com>
2017-12-05 13:12:05 -0500

    database created

--- manage.py
+++ manage.py
@@ -1,17 +1,28 @@
 from flask_script import Manager
-from project import app, db
+from project import app, db, Manufacturer, Car
+
 
 manager = Manager(app)
 
 
-@manager.command
+@manager.command ## populate database with real data
 def deploy():
     db.drop_all()
     db.create_all()
-    x = X()
-    db.session.add()
+    man1 = Manufacturer(manuf='BMW', country='Germany')
+    man2 = Manufacturer(manuf='Mercedes', country='Germany')
+    man3 = Manufacturer(manuf='Tesla', country='United States of America')
+    car1 = Car(model='M6', type='Coupe', year='2015', manuf='BMW', description="One of BMWs best performance cars.")
+    car2 = Car(model='S Class', type='Sedan', year='2013', manuf='Mercedes', description="The ultimate Mercedes in terms of luxary.")
+    car3 = Car(model='Model 3', type='Electric', year='2018', manuf='Tesla', description="The worlds first premium electric car under $40,000.")
+    db.session.add(man1)
+    db.session.add(man2)
+    db.session.add(man3)
+    db.session.add(car1)
+    db.session.add(car2)
+    db.session.add(car3)
     db.session.commit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
